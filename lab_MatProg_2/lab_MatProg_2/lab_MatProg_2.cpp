6bd90dc252709932d2332e57f4bac760af333669
ConsciencE13 <105731307+ConsciencE13@users.noreply.github.com>
2023-02-17 18:41:41 +0300

    Add files via upload

---
+++ lab_MatProg_2/lab_MatProg_2/lab_MatProg_2.cpp
@@ -0,0 +1,193 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include "Combi.h"
+#include "Knapsack.h"
+#define N (sizeof(AA)/2)
+#define M 3
+#define NN 20
+//#define Subset
+//#define Combination
+//#define Permutation
+//#define Accomodations
+#define Knapsack
+int main()
+{
+#ifdef Subset
+    setlocale(LC_ALL, "rus");
+    char  AA[][2] = { "A", "B", "C", "D", "E" };
+    std::cout << " - Генератор множества всех подмножеств -" << std::endl;
+    std::cout << "Исходное множество: ";
+    std::cout << "{ ";
+    for (int i = 0; i < sizeof(AA) / 2; i++)
+        std::cout << AA[i] << ((i < sizeof(AA) / 2 - 1) ? ", " : " ");
+    std::cout << "}";
+    std::cout << std::endl << "Генерация всех подмножеств  ";
+    combi::subset s1(sizeof(AA) / 2);         // создание генератора   
+    int n = s1.getfirst();                // первое (пустое) подмножество    
+    while (n >= 0)                          // пока есть подмножества 
+    {
+        std::cout << std::endl << "{ ";
+        for (int i = 0; i < n; i++)
+            std::cout << AA[s1.ntx(i)] << ((i < n - 1) ? ", " : " ");
+        std::cout << "}";
+        n = s1.getnext();                   // cледующее подмножество 
+    }
+    std::cout << std::endl << "всего: " << s1.count() << std::endl;
+    system("pause");
+    return 0;
+#endif
+
+#ifdef Combination
+    setlocale(LC_ALL, "rus");
+    char  AA[][2] = { "A", "B", "C", "D", "E", "F" };
+    std::cout << std::endl << " --- Генератор сочетаний ---";
+    std::cout << std::endl << "Исходное множество: ";
+    std::cout << "{ ";
+    for (int i = 0; i < sizeof(AA) / 2; i++)
+
+        std::cout << AA[i] << ((i < sizeof(AA) / 2 - 1) ? ", " : " ");
+    std::cout << "}";
+    std::cout << std::endl << "Генерация сочетаний ";
+    combi::xcombination xc(sizeof(AA) / 2, 2);
+    std::cout << "из " << xc.n << " по " << xc.m;
+    int  n = xc.getfirst();
+    while (n >= 0)
+    {
+        std::cout << std::endl << xc.nc << ": { ";
+        for (int i = 0; i < n; i++)
+            std::cout << AA[xc.ntx(i)] << ((i < n - 1) ? ", " : " ");
+        std::cout << "}";
+        n = xc.getnext();
+    };
+    std::cout << std::endl << "всего: " << xc.count() << std::endl;
+    system("pause");
+    return 0;
+#endif
+
+#ifdef Permutation
+    setlocale(LC_ALL, "rus");
+    char  AA[][2] = { "A", "B" };
+    std::cout << std::endl << " --- Генератор перестановок ---";
+    std::cout << std::endl << "Исходное множество: ";
+    std::cout << "{ ";
+    for (int i = 0; i < sizeof(AA) / 2; i++)
+
+        std::cout << AA[i] << ((i < sizeof(AA) / 2 - 1) ? ", " : " ");
+    std::cout << "}";
+    std::cout << std::endl << "Генерация перестановок ";
+    combi::permutation p(sizeof(AA) / 2);
+    __int64  n = p.getfirst();
+    while (n >= 0)
+    {
+        std::cout << std::endl << std::setw(4) << p.np << ": { ";
+
+        for (int i = 0; i < p.n; i++)
+
+            std::cout << AA[p.ntx(i)] << ((i < p.n - 1) ? ", " : " ");
+
+        std::cout << "}";
+
+        n = p.getnext();
+    };
+    std::cout << std::endl << "всего: " << p.count() << std::endl;
+    system("pause");
+    return 0;
+
+#endif
+
+#ifdef Accomodations
+    setlocale(LC_ALL, "rus");
+    char  AA[][2] = { "A", "B", "C", "D", "E" };
+    std::cout << std::endl << " --- Генератор размещений ---";
+    std::cout << std::endl << "Исходное множество: ";
+    std::cout << "{ ";
+    for (int i = 0; i < N; i++)
+
+        std::cout << AA[i] << ((i < N - 1) ? ", " : " ");
+    std::cout << "}";
+    std::cout << std::endl << "Генерация размещений  из  " << N << " по " << M;
+    combi::accomodation s(N, M);
+    int  n = s.getfirst();
+    while (n >= 0)
+    {
+
+        std::cout << std::endl << std::setw(2) << s.na << ": { ";
+
+        for (int i = 0; i < 3; i++)
+
+            std::cout << AA[s.ntx(i)] << ((i < n - 1) ? ", " : " ");
+
+        std::cout << "}";
+
+        n = s.getnext();
+    };
+    std::cout << std::endl << "всего: " << s.count() << std::endl;
+    system("pause");
+    return 0;
+
+#endif
+
+#ifdef Knapsack
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    int V = 300;               // вместимость рюкзака              
+    int v[NN];     // размер предмета каждого типа  
+    int c[NN];     // стоимость предмета каждого типа 
+    short m[NN];                // количество предметов каждого типа  {0,1}   
+
+    for (int i = 0; i < NN; i++)
+    {
+        v[i] = rand() % (300 + 1 - 10) + 10;
+        c[i] = rand() % (55 + 1 - 5) + 5;
+    }
+    int maxcc = knapsack_s(
+
+        V,   // [in]  вместимость рюкзака 
+        NN,  // [in]  количество типов предметов 
+        v,   // [in]  размер предмета каждого типа  
+        c,   // [in]  стоимость предмета каждого типа     
+        m    // [out] количество предметов каждого типа  
+    );
+
+    /*std::cout << std::endl << "-------- Задача о рюкзаке --------- ";
+    std::cout << std::endl << "- количество предметов : " << NN;
+    std::cout << std::endl << "- вместимость рюкзака  : " << V;
+    std::cout << std::endl << "- размеры предметов    : ";
+    for (int i = 0; i < NN; i++) std::cout << v[i] << " ";
+    std::cout << std::endl << "- стоимости предметов  : ";
+    for (int i = 0; i < NN; i++) std::cout << v[i] * c[i] << " ";
+    std::cout << std::endl << "- оптимальная стоимость рюкзака: " << maxcc;
+    std::cout << std::endl << "- вес рюкзака: ";
+    int s = 0; for (int i = 0; i < NN; i++) s += m[i] * v[i];
+    std::cout << s;
+    std::cout << std::endl << "- выбраны предметы: ";
+    for (int i = 0; i < NN; i++) std::cout << " " << m[i];
+    std::cout << std::endl << std::endl;
+
+    system("pause");
+    return 0;*/
+
+
+
+    clock_t t1, t2;
+    std::cout << std::endl << "-------- Задача о рюкзаке --------- ";
+    std::cout << std::endl << "- вместимость рюкзака  : " << V;
+    std::cout << std::endl << "-- количество ------ продолжительность -- ";
+    std::cout << std::endl << "    предметов          вычисления  ";
+    for (int i = 12; i <= NN; i++)
+    {
+        t1 = clock();
+        maxcc = knapsack_s(V, i, v, c, m);
+        t2 = clock();
+        std::cout << std::endl << "       " << std::setw(2) << i
+            << "               " << std::setw(5) << (t2 - t1);
+    }
+    std::cout << std::endl << std::endl;
+    system("pause");
+    return 0;
+
+#endif
+
+}
+
